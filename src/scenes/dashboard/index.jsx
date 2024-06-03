5da40511a6b85212c7ee2e9c9a766e5a443d9e7e
Siqah <faruoqmuhammed@zetech.ac.ke>
2024-06-03 23:56:02 +0300

    header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,11 @@
+import Header from "../../components/Header"
+import {Box} from "@mui/material"
 
 const  Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Box>
+      <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+    </Box>
   )
 }
 
