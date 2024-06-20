57225babedccd6652ae3fe9d479e47a675bff74b
Siqah <faruoqmuhammed@zetech.ac.ke>
2024-06-20 13:18:12 +0300

    GeographyChart done

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
 function App() {
@@ -40,7 +40,7 @@ function App() {
                 <Route path="/line" element={<Line />} />
                 <Route path="/pie" element={<Pie />} />
                 <Route path="/faq" element={<FAQ />} />
-                {/* <Route path="/geography" element={<Geography />} /> */}
+                <Route path="/geography" element={<Geography />} />
                 <Route path="/calendar" element={<Calendar />} />
 
                </Routes>
